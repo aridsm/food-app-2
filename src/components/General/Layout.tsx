b30ee30311a96278ba61b8d31f91ad353da9cde3
aridsm <aniemorelanie@gmail.com>
2023-08-16 20:14:02 -0300

    fix: ajuste responsivo da tela home

--- src/components/General/Layout.tsx
+++ src/components/General/Layout.tsx
@@ -8,7 +8,7 @@ const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div>
       <SubHeaderTop />
-      <div className="flex flex-col min-h-screen h-full md:max-w-[820px] xl:max-w-[980px] mx-auto px-2 py-4 ">
+      <div className="flex flex-col min-h-screen h-full md:max-w-[820px] xl:max-w-[980px] 2xl:max-w-[1060px] mx-auto px-2 py-4 ">
         <Header />
         <main className="flex-1 py-6 md:py-10">
           <ArcSvg className="hidden lg:block absolute top-2/3 -left-[70px] rotate-90" />
